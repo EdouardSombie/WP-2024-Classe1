5d2ee615f4d9477f3a8676cf038eba5e7d9f26fe
Edouard <edouard@voltajazz.com>
2024-04-19 18:52:41 +0200

    Fin premiere séance TP thème classic

---
+++ wordpress-1/ESGI-theme/template-parts/identity-card.php
@@ -0,0 +1,29 @@
+<article class="identity-card">
+    <?= get_custom_logo() ?>
+    <h1><?= get_bloginfo('name') ?></h1>
+    <h2><?= get_bloginfo('description') ?></h2>
+    <footer>
+        <ul>
+            <li>
+                <a href="#">
+                    <?= esgi_get_icon('twitter'); ?>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <?= esgi_get_icon('facebook'); ?>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <?= esgi_get_icon('google'); ?>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <?= esgi_get_icon('linkedin'); ?>
+                </a>
+            </li>
+        </ul>
+    </footer>
+</article>
